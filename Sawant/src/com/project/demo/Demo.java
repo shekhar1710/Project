f0dfa540c1c659f5cbc736cd94041146ec499dd3
shekhar1710 <sawantshekhar1717@gmail.com>
2022-09-14 18:25:45 +0530

    Arraylist program added

---
+++ Sawant/src/com/project/demo/Demo.java
@@ -0,0 +1,23 @@
+package com.project.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+	List<String> list = new ArrayList<>();
+	list.add("Krushnat");
+	list.add("Rohit");
+	list.add("Rushikesh");
+	list.add("Gayatri");
+	
+	for ( String g : list) {
+		System.out.println(g);
+	}
+
+	}
+
+}
